08f32062e9173b8f42e6b22b0af412ba94008f0f
Hakan Bamov <bamovh@icloud.com>
2020-09-15 22:29:22 +0300

    implement working chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,11 +4,22 @@ import io from 'socket.io-client';
 
 import './Chat.css'
 
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from '../Messages/Messages'
+import TextContainer from "../TextContainer/TextContainer";
+
 let socket;
 
 const Chat = ({location}) => {
+    // eslint-disable-next-line
     const [name, setName] = useState('');
+    // eslint-disable-next-line
     const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([])
+
     const ENDPOINT = 'localhost:5000'
 
     useEffect(() => {
@@ -20,7 +31,6 @@ const Chat = ({location}) => {
         setRoom(room);
 
         socket.emit('join', { name, room }, () => {
-
         })
 
         return () => {
@@ -30,8 +40,41 @@ const Chat = ({location}) => {
         }
     }, [ENDPOINT, location.search])
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            console.log('appending')
+            setMessages([...messages, message]);
+        })
+
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+        });
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages)
+
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name}/>
+                <Input
+                    message={message}
+                    setMessage={setMessage}
+                    sendMessage={sendMessage}
+                />
+
+            </div>
+            <TextContainer users={users} />
+        </div>
     )
 }
 
